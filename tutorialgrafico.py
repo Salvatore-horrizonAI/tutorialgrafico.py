300ced265393fc6285694f76f96e3a65cf224f02
Salvatore Naro <salvatorenaro009@gmail.com>
2025-04-18 23:49:30 +0200

    Update tutorialgrafico.py

--- tutorialgrafico.py
+++ tutorialgrafico.py
@@ -1,15 +1,14 @@
-
-import matplotlib.pyplot as plt #importiamo la libreria matplotlip per il grafico
-
-array = [1,2,3,4,5,6,7]#creiamo una matrice(quadro di numeri)
+#tutorial Salvatore Naro
+import matplotlib.pyplot as plt 
+array = [1,2,3,4,5,6,7]
 plt.figure(1,figsize=(7,4),facecolor="black")#creiamo il grafico e li diamo delle dimensioni e il colore
-plt.plot(array,marker="o",color="cyan")#impostiamo la traiettoria,definendo di voler tracciare la variabile array,usiamo marker per i vari punti e li diamo alla traiettoria il colore cyan
-plt.xlabel("X",color="white")#creiamo i label per vedere cosa rappresenta il grafico
+plt.plot(array,marker="o",color="cyan")
+plt.xlabel("X",color="white")
 plt.ylabel("Y",color="white")
-plt.grid(True,color="Gray",linestyle="--",alpha=0.5)#impostiamo una griglia di colore grigia e trattegiata
-plt.axhline(0,color="Gray")#la traiettoria parte dal punto 0
+plt.grid(True,color="Gray",linestyle="--",alpha=0.5)
+plt.axhline(0,color="Gray")
 plt.legend()
 a = plt.gca()
-a.set_facecolor('black')#impostazioni default
-a.tick_params(colors='white')#default
-plt.show()#per vedere il grafico
+a.set_facecolor('black')
+a.tick_params(colors='white')
+plt.show()
